1701128fd954493f9ba27b9606a41e1e2bf596b8
axitchandora <axitchandora@gmail.com>
2022-05-22 17:01:57 +0530

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # ME_POLYMORPHISM_JAVA
+
 A Project Repository for Polymorphism Byte in Java
---
+++ messaging/src/main/java/com/crio/messaging/MessagingApplication.java
@@ -0,0 +1,13 @@
+package com.crio.messaging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessagingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessagingApplication.class, args);
+	}
+
+}
